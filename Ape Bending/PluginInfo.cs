54ae7f02472a0702fd17fb14d66386aa5a0df4d5
JosFa1 <joesepicthings@gmail.com>
2023-07-20 15:04:45 +0200

    Test

---
+++ Ape Bending/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Ape_Bending
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.josfa.gorillatag.ape bending";
+        public const string Name = "Ape_Bending";
+        public const string Version = "1.0.0";
+    }
+}
